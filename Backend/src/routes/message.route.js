e7dc8f9f9896d7a2066bbd6d45b9b7faf0712487
Ranit Mondal <ranitmondal197@gmail.com>
2026-06-02 23:42:32 +0530

    massege route

--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -1,9 +1,12 @@
 import express from 'express';
-
+import { getAllContects, getAllPatners, getMessagesByUserId, sendMessage } from '../controller/message.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 const router = express.Router();
 
-router.get("/send", (req, res) => {
-  res.send("Messages route");
-});
+router.get("/contects", protectRoute, getAllContects);
+router.get("/chats", protectRoute, getAllPatners);
+router.get("/:id", protectRoute, getMessagesByUserId);
+
+router.post("/send/:id", protectRoute, sendMessage );
 
 export default router;
