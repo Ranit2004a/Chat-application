003b4be4eba1510ae9bc473ee91c1b51f76cb610
Ranit Mondal <ranitmondal197@gmail.com>
2026-06-07 18:40:30 +0530

    cors error fix

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -11,7 +11,7 @@ export  const useAuthStore = create((set) => ({
             const res = await axiosInstance.get("/auth/check")
             set({ authUser: res.data})
         } catch (error) {
-            consolelog("Error checking auth:", error);
+            console.log("Error checking auth:", error);
             set({ authUser: null });
         }finally {
             set({ isCheckingAuth: false });
